bae623ab43a4ea28f1a6971994ba03d558466c21
Emily Resch <eresch527@gmail.com>
2020-05-11 15:47:22 -0500

    fixed license

--- index.js
+++ index.js
@@ -133,7 +133,8 @@ ${answers.tests}
 ${answers.questions}
 
 ## License
-${answers.title} (C) ${answers.year}, ${answers.authors}
+${answers.title} (C) ${answers.year}, ${answers.authors}.
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
